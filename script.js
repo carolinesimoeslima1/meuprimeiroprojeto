26e9b94357542cf9e953f45e151446160cc913ef
carolinesimoeslima1 <carolinesimoeslima@hotmail.com>
2026-07-23 22:52:42 +0000

    adiociona botão

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-alert("Olá, JavaScript!");
+const botao = document.getElementById("meuBotao");
+const mensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click", function() {
+  mensagem.textContent = "Você clicou no botão!";
+});
