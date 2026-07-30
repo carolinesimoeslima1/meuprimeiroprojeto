70bd78359941a762a66889315d815030c7001ff7
carolinesimoeslima1 <carolinesimoeslima@hotmail.com>
2026-07-30 12:23:16 +0000

    adicionando função

--- script.js
+++ script.js
@@ -2,14 +2,21 @@ const botao = document.getElementById("meuBotao");
 const mensagem = document.getElementById("mensagem");
 const nomeInput = document.getElementById("nomeInput");
 
+function validarNome(nome) {
+  return nome.trim() !== "";
+}
+
+function exibirMensagem(nome) {
+  if (validarNome(nome)) {
+    mensagem.textContent = "Olá, " + nome + "! Obrigada por clicar.";
+  } else {
+    mensagem.textContent = "Por favor, digite seu nome!";
+  }
+}
+
 botao.addEventListener("click", function() {
   const nome = nomeInput.value;
-
-  if (nome === "") {
-    mensagem.textContent = "Por favor, digite seu nome!";
-  } else {
-    mensagem.textContent = "Olá, " + nome + "! Obrigada por clicar.";
-  }
+  exibirMensagem(nome);
 });
 const idade = 20;
 console.log(idade);
